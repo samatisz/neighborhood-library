649cb33b49adec350246c0292a6e8937495343b8
samatisz <shelbymatisz@gmail.com>
2024-04-24 20:37:58 -0400

    updated library

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,4 +1,85 @@
 package com.pluralsight;
 
 public class Book {
-}
+     private int id;
+     private String isbn;
+     private String title;
+     private boolean isCheckedOut;
+     private String checkedOutTo;
+
+     public Book(int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
+          this.id = id;
+          this.isbn = isbn;
+          this.title = title;
+          this.isCheckedOut = isCheckedOut;
+          this.checkedOutTo = checkedOutTo;
+     }
+
+     public int getId() {
+          return id;
+     }
+
+     public void setId(int id) {
+          this.id = id;
+     }
+
+     public String getIsbn() {
+          return isbn;
+     }
+
+     public void setIsbn(String isbn) {
+          this.isbn = isbn;
+     }
+
+     public String getTitle() {
+          return title;
+     }
+
+     public void setTitle(String title) {
+          this.title = title;
+     }
+
+     public boolean isCheckedOut() {
+          return isCheckedOut;
+     }
+
+     public void setCheckedOut(boolean checkedOut) {
+          isCheckedOut = checkedOut;
+     }
+
+     public String getCheckedOutTo() {
+          return checkedOutTo;
+     }
+
+     public void setCheckedOutTo(String checkedOutTo) {
+          this.checkedOutTo = checkedOutTo;
+     }
+
+
+ public void checkOut(String name) {
+      if (!isCheckedOut) {
+           isCheckedOut = true;
+           checkedOutTo = name;
+           System.out.println(title + " has been checked out to " + name);
+      } else {
+           System.out.println(title + " is already checked out.");
+      }
+ }
+
+ public void checkIn() {
+               if (isCheckedOut) {
+                    isCheckedOut = false;
+                    checkedOutTo = "";
+                    System.out.println(title + " has been checked in.");
+               } else {
+                    System.out.println(title + " is not checked out.");
+
+               }
+
+
+          }
+
+ }
+
+
+
--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -1,7 +1,31 @@
 package com.pluralsight;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class NeighborhoodLibrary {
     public static void main(String[] args) {
+        Book[] books = new Book[] {
+                new Book(1, "978-0-547-92821-0", "The Lord of the Rings: The Fellowship of the Ring", false, ""),
+                new Book(2, "978-0-7679-3061-1", "Breathers: A Zombie's Lament", false, ""),
+                new Book(3,"978-0-099-58772-9", "The New Hunger", false, ""),
+                new Book(4, "978-1-4767-1746-3", "Warm Bodies", false, ""),
+                new Book(5, "978-0-307-34661-2", "World War Z: An Oral History of the Zombie War", false, ""),
+                new Book(6, "978-0-756-40675-2", "My Life as a White Trash Zombie", false, ""),
+                new Book(7,"978-1-7365009-8-9", "The Little Mushroom: Judgment Day", false, ""),
+                new Book(8, "978-10956609-99-8", "The Little Mushroom: Revelations", false, ""),
+                new Book(9,"979-8-88843-261-7", "The Disabled Tyrant's Beloved Pet Fish", false, ""),
+                new Book(10,"978-1-64827-917-1", "Heaven Official's Blessing: Book 1", false, ""),
+        };
+
+
+        Scanner myScanner = new Scanner(System.in);
+
+
+
+        myScanner.close();
+
+        
 
     }
 }
