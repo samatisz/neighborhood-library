1b6bf99e8a64a14f2ec89ac94720cbbfa816983e
samatisz <shelbymatisz@gmail.com>
2024-04-25 15:55:32 -0400

    added more books

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -7,16 +7,19 @@ import java.util.Scanner;
 public class NeighborhoodLibrary {
 
    private static Book[] books = new Book[] {
-            new Book(1, "978-0-547-92821-0", "The Lord of the Rings: The Fellowship of the Ring", false, ""),
-            new Book(2, "978-0-7679-3061-1", "Breathers: A Zombie's Lament", false, ""),
-            new Book(3,"978-0-099-58772-9", "The New Hunger", false, ""),
-            new Book(4, "978-1-4767-1746-3", "Warm Bodies", false, ""),
-            new Book(5, "978-0-307-34661-2", "World War Z: An Oral History of the Zombie War", false, ""),
-            new Book(6, "978-0-756-40675-2", "My Life as a White Trash Zombie", false, ""),
-            new Book(7,"978-1-7365009-8-9", "The Little Mushroom: Judgment Day", false, ""),
-            new Book(8, "978-10956609-99-8", "The Little Mushroom: Revelations", false, ""),
-            new Book(9,"979-8-88843-261-7", "The Disabled Tyrant's Beloved Pet Fish", false, ""),
-            new Book(10,"978-1-64827-917-1", "Heaven Official's Blessing: Book 1", false, ""),
+            new Book(1, "978-0-547-92821-0", "The Lord of the Rings: The Fellowship of the Ring by J.R.R Tolkien", false, ""),
+            new Book(2, "978-0-7679-3061-1", "Breathers: A Zombie's Lament by S. G. Browne", false, ""),
+            new Book(3,"978-0-099-58772-9", "The New Hunger by Isaac Marion", false, ""),
+            new Book(4, "978-1-4767-1746-3", "Warm Bodies by Isaac Marion", false, ""),
+            new Book(5, "978-0-307-34661-2", "World War Z: An Oral History of the Zombie War by Max Brooks", false, ""),
+            new Book(6, "978-0-756-40675-2", "My Life as a White Trash Zombie by Diana Rowland", false, ""),
+            new Book(7,"978-1-7365009-8-9", "The Little Mushroom: Judgment Day by Shi Si", false, ""),
+            new Book(8, "978-10956609-99-8", "The Little Mushroom: Revelations by Shi Si", false, ""),
+            new Book(9,"979-8-88843-261-7", "The Disabled Tyrant's Beloved Pet Fish by Xue Shan Fei Yu", false, ""),
+            new Book(10,"978-1-64827-917-1", "Heaven Official's Blessing: Book 1 by Mo Xiang Tong Xu", false, ""),
+            new Book(11, "978-0-39472-274-0", "Son of the Revolution by Liang Heng", false, ""),
+            new Book(12, "978-0-19581-139-1", "Treasure Island by Robert Louis Stevenson", false, "")
+
     };
 
 
